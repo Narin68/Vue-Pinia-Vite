65fc30e973bdbdc91028259d15e9ac062e6a83ad
Narin68 <pachnarin68@gmail.com>
2023-03-21 17:20:04 +0700

    new

---
+++ vue-composition-api-basic/src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="about"><h1>About</h1></h1>
+    <div>
+      <input type="text" v-autofocus />
+    </div>
+  </div>
+</template>
+<script setup>
+import { vAutofocus } from "../derectives/vAutofocus.js";
+</script>
+<style>
+</style>
---
+++ vue-composition-api-basic/src/views/HomeView.vue
@@ -0,0 +1,45 @@
+<script setup>
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{ counterData.title }}</h1>
+
+    <button @click="decrease" class="btn">-</button>
+    <span class="counter">{{ counterData.counter }}</span>
+    <button @click="increase" class="btn">+</button>
+    <div>
+      <input type="text" v-autofocus />
+    </div>
+  </div>
+</template>
+<script setup>
+import { reactive, ref } from "vue";
+import { vAutofocus } from "../derectives/vAutofocus.js";
+// const counter = ref(0);
+
+const counterData = reactive({
+  counter: 0,
+  tittle: "My Counter",
+});
+
+const decrease = () => {
+  counterData.counter--;
+};
+const increase = () => {
+  counterData.counter++;
+};
+</script>
+<style scoped>
+.home {
+  /* text-align: center; */
+  padding: 20px;
+}
+.btn {
+  padding: 15px;
+  font-size: 20px;
+}
+.counter {
+  margin: 0 20px;
+}
+</style>
